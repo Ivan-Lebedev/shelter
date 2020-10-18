f9ff00a2c5569ae9c393b94284a4e86f87df00e8
ivan-lebedev <ivieee@yandex.ru>
2020-10-18 17:29:20 +0400

    add burger-menu to main page

---
+++ pages/main/script.js
@@ -0,0 +1,19 @@
+const header__burger = document.querySelector('.burger-button')
+const header_menu = document.querySelector('.header-nav')
+const back = document.querySelector('body')
+
+header__burger.onclick = toggleMenu = () => {
+    header__burger.classList.toggle('active')
+    header_menu.classList.toggle('active')
+    back.classList.toggle('lock')
+}
+
+document.addEventListener('click', e => {
+    const target = e.target
+    const menu = target === header_menu || header_menu.contains(target)
+    const burgerBtn = target === header__burger || header__burger.contains(target)
+    const menuIsActive = header_menu.classList.contains('active')
+    if (!menu && !burgerBtn && menuIsActive) {
+        toggleMenu() 
+    }
+})
